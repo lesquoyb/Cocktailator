a9d1b630d6c839fe15678245d75b75b1278f3e67
Mattew <mattewphoenyx@hotmail.fr>
2014-10-28 01:34:20 +0100

    Connexion_5 (css update)

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
 require_once("php/functions.php");
 ?>
+<!DOCTYPE html>
+<html>
 <head>
 	<meta charset="utf-8">
 	<meta name="title" content="la CCR, Jeu de stratégie en ligne multi-joueur">
 	<meta name="description" content="Jeu de stratégie en ligne">
-	<meta name=viewport content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Amaranth">
 	<link type="image/png" href="Graphics/icon.png" rel="icon">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
-	<link href="css/bootstrap-modif.css" rel="stylesheet">
 	<link href="Heyleon/css/heyleon.css" rel="stylesheet">
 	<link href="css/general.css" rel="stylesheet">
-	<link href="css/chat.css" rel="stylesheet">
 	<script src="js/jquery.js"></script>
 	<script async src="js/bootstrap.min.js "></script>
 	<script async src="js/md5.js"></script>
@@ -25,46 +25,47 @@ require_once("php/functions.php");
 </body>
 
 <div class="box">
-<?php
-if (isGet('error', $err)) {
-	if ($err == 'login') {
-		$title_error = "Compte incorrect :";
-		$text_error = "Personne ne s'est inscrit avec ce pseudo.";
-	} elseif ($err == 'password') {
-		$title_error = "Compte incorrect :";
-		$text_error = "Le mot de passe saisi est incorrect.";
-	} elseif ($err == 'pseudo_exist') {
-		$title_error = "Données utilisées :";
-		$text_error = "Le pseudo entré est déjà utilisé.";
-	} elseif ($err == 'mail_exist') {
-		$title_error = "Données utilisées :";
-		$text_error = "L'adresse mail entrée est déjà liée à un compte.";
-	} elseif ($err == 'expire') {
-		$title_error = "Session expirée :";
-		$text_error = "Vous avez été déconnecté automatiquement, connectez-vous pour continuer.";
-	} elseif ($err == 'disconnected') {
-		$title_error = "Authentification requise :";
-		$text_error = "Vous devez vous connecter pour accéder à cette page !";
-	} elseif ($err == 'protected') {
-		$title_error = "Authentification requise :";
-		$text_error = "Vous n'êtes pas autorisés à accéder à cette page.";
-	} else {
-		$title_error = "Erreur détectée :";
-		$text_error = "Une erreur a eu lieu lors de l'authentification. Nom de l'erreur : <strong>".$err."</strong>";
+	<?php
+	if (isGet('error', $err)) {
+		if ($err == 'login') {
+			$title_error = "Compte incorrect :";
+			$text_error = "Personne ne s'est inscrit avec ce pseudo.";
+		} elseif ($err == 'password') {
+			$title_error = "Compte incorrect :";
+			$text_error = "Le mot de passe saisi est incorrect.";
+		} elseif ($err == 'pseudo_exist') {
+			$title_error = "Données utilisées :";
+			$text_error = "Le pseudo entré est déjà utilisé.";
+		} elseif ($err == 'mail_exist') {
+			$title_error = "Données utilisées :";
+			$text_error = "L'adresse mail entrée est déjà liée à un compte.";
+		} elseif ($err == 'expire') {
+			$title_error = "Session expirée :";
+			$text_error = "Vous avez été déconnecté automatiquement, connectez-vous pour continuer.";
+		} elseif ($err == 'disconnected') {
+			$title_error = "Authentification requise :";
+			$text_error = "Vous devez vous connecter pour accéder à cette page !";
+		} elseif ($err == 'protected') {
+			$title_error = "Authentification requise :";
+			$text_error = "Vous n'êtes pas autorisés à accéder à cette page.";
+		} else {
+			$title_error = "Erreur détectée :";
+			$text_error = "Une erreur a eu lieu lors de l'authentification. Nom de l'erreur : <strong>".$err."</strong>";
+		}
+		echo '<div class="alert fade in"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><h4>'.$title_error.'</h4>'.$text_error.'</div>';
 	}
-	echo '<div class="alert fade in"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><h4>'.$title_error.'</h4>'.$text_error.'</div>';
-}
-if (isGet('action', $action)) {
-	if ($action == 'registered') {
-		$title_action = "Enregistrement réussi :";
-		$text_action = "Votre enregistrement a bien été pris en compte. Un mail vient d'être envoyé à l'adresse renseignée sur le formulaire.";
-	} 
-	echo '<div class="success fade in"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><h4>'.$title_action.'</h4>'.$text_action.'</div>';
-}
-echo "</div>";
-?>
+	if (isGet('action', $action)) {
+		if ($action == 'registered') {
+			$title_action = "Enregistrement réussi :";
+			$text_action = "Votre enregistrement a bien été pris en compte. Un mail vient d'être envoyé à l'adresse renseignée sur le formulaire.";
+		} 
+		echo '<div class="success fade in"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><h4>'.$title_action.'</h4>'.$text_action.'</div>';
+	}
+	?>
+</div>
 
-	<div style="margin:auto;width:70%;color:#fff;font-size:18px;text-shadow: 1px 1px black;">
+
+	<div style="margin:auto;width:70%;color:#fff;font-size:18px;text-shadow: 1px 1px black;padding-top: 6%;">
 		<h4 style="text-align:center;font-size:20px;text-decoration:underline;">Bienvenue sur Cocktailator !</h4>
 		<p>C'est un site superbe !</p>
 	</div>
@@ -77,3 +78,4 @@ echo "</div>";
 		else echo "$('#enter').load('_index/login.php');"; ?>
 	</script>
 </body>
+</html>
