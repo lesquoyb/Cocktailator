12d746cd46a258784b1107086ad0bd155b29dafd
Mattew <mattewphoenyx@hotmail.fr>
2014-10-28 01:19:08 +0100

    Connexion_4 (css)

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ require_once("php/functions.php");
 	<meta name="title" content="la CCR, Jeu de stratégie en ligne multi-joueur">
 	<meta name="description" content="Jeu de stratégie en ligne">
 	<meta name=viewport content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Amaranth">
 	<link type="image/png" href="Graphics/icon.png" rel="icon">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
@@ -17,7 +18,7 @@ require_once("php/functions.php");
 	<script async src="js/bootstrap.min.js "></script>
 	<script async src="js/md5.js"></script>
 	<title>Cocktailator</title>
-	<meta name="keywords" content="online, game, CCR, ascaze">
+	<meta name="keywords" content="cocktail, ator">
 	<meta name="robots" content="index, nofollow" >
 </head>
 
