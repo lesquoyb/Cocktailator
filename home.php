9b328375e023cd6be1ccfa995afd0069445a7c1f
Mattew <mattewphoenyx@hotmail.fr>
2014-10-30 17:40:26 +0100

    Home (1)

---
+++ home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="title" content="la CCR, Jeu de stratégie en ligne multi-joueur">
+	<meta name="description" content="Jeu de stratégie en ligne">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Amaranth">
+	<link type="image/png" href="Graphics/icon.png" rel="icon">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+	<link href="Heyleon/css/heyleon.css" rel="stylesheet">
+	<link href="css/general.css" rel="stylesheet">
+	<script src="js/jquery.js"></script>
+	<script async src="js/functions.js"></script>
+	<script async src="js/bootstrap.min.js "></script>
+	<script async src="js/md5.js"></script>
+	<title>Cocktailator</title>
+	<meta name="keywords" content="cocktail, ator">
+	<meta name="robots" content="index, nofollow" >
+</head>
+
+</body>
+
+<?php include("menu.php"); ?>
+
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,12 @@
+<!-- Top menu -->
+<nav class="navbar navbar-inverse navbar-default navbar-fixed-top">
+	<div class="container">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="#">Cocktailator</a>
+		</div>
+		<ul class="nav navbar-nav">
+			<li><a href="#"> Cocktails </a></li>
+			<li><a href="#"> Ingrédients </a></li>
+		</ul>
+	</div>
+</nav>
