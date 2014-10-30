39f84092ad9a17511b975f1ed64cef9350e027d2
Mattew <mattewphoenyx@hotmail.fr>
2014-10-30 20:08:03 +0100

    home (2)

--- menu.php
+++ menu.php
@@ -1,12 +1,14 @@
 <!-- Top menu -->
-<nav class="navbar navbar-inverse navbar-default navbar-fixed-top">
-	<div class="container">
-		<div class="navbar-header">
-			<a class="navbar-brand" href="#">Cocktailator</a>
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+	<div class="container" style="height:70px;">
+		<div class="navbar-header"><img style="height: 70px;" src="Graphics/logo.png"/></div>
+		
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav">
+				<li><a href="#">Cocktails</a></li>
+				<li><a href="#">Ingrédients</a></li>
+			</ul>
 		</div>
-		<ul class="nav navbar-nav">
-			<li><a href="#"> Cocktails </a></li>
-			<li><a href="#"> Ingrédients </a></li>
-		</ul>
+
 	</div>
 </nav>
