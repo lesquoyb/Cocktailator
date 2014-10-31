237792a8229d4b778165cd2a647b76b49803b901
lesquoyb <nouvelleaddresse@yahoo.fr>
2014-10-31 02:08:20 +0100

    les photooooooos

---
+++ user/index.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../php/functions.php';
+onlyRegistered();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		
+		<?php include '../links.html'; ?>
+
+		<title><?= $_SESSION["pseudo"]?>: espace perso</title>	</head>
+
+
+	<body>
+		<?php include("../menu.php"); ?>
+		<div class="middle_container">
+			
+		</div>
+
+	</body>
+</html>
