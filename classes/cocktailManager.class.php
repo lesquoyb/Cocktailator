3be1597b6301d211b880cf36e7e3ef4a47262649
Mattew <mattewphoenyx@hotmail.fr>
2014-12-14 13:56:57 +0100

    Commentaires R�ussis :8ball:

--- classes/cocktailManager.class.php
+++ classes/cocktailManager.class.php
@@ -142,7 +142,7 @@ class CocktailManager implements DAO{
 	* Retourne les commentaires associés à $cocktail sous forme html.
 	*/
 	public function commentaires(Cocktail $cocktail){
-		$query =  $this->_db->query("SELECT * from comments , user WHERE id_cocktail = $cocktail->_id");
+		$query =  $this->_db->query("SELECT user_login, title, comment FROM comments c, user u WHERE id_cocktail = $cocktail->_id AND c.id_user = u.id_user");
 		$com = "";
 		foreach ($query->fetchAll() as $key => $value) { 
 			$com .= "<div class='com'><div class='auteur'>";
